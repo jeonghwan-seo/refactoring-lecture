c32bd68e9546e1b16a73728a629d496d82262900
jeonghwan <jeonghwan.se@mincoding.co.kr>
2024-09-29 10:54:22 +0900

    조건문 분해하기 - Employee

---
+++ src/main/java/SplitConditionalTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+public class SplitConditionalTest {
+}
+
+class Employee {
+    private String employeeType;
+    private double basePayRate;
+    public double calculatePay(boolean isOvertime) {
+        double pay;
+        if (isOvertime) {
+            if ("FullTime".equals(employeeType)) {
+                pay = basePayRate * 1.5;
+            } else if ("PartTime".equals(employeeType)) {
+                pay = basePayRate * 1.25;
+            } else {
+                pay = basePayRate;
+            }
+            return pay;
+        }
+        else {
+            if ("FullTime".equals(employeeType) || "PartTime".equals(employeeType)) {
+                pay = basePayRate;
+            } else if ("Contractor".equals(employeeType)) {
+                pay = basePayRate * 0.9;
+            } else {
+                pay = basePayRate * 0.8;
+            }
+            return pay;
+        }
+    }
+}
+
