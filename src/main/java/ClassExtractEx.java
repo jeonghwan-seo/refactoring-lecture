9a65c2256f2b699005f1fa7ae3c2d278084f1759
jeonghwan <jeonghwan.se@mincoding.co.kr>
2024-09-29 10:19:10 +0900

    클래스 추출 Person - Address

---
+++ src/main/java/ClassExtractEx.java
@@ -0,0 +1,18 @@
+public class ClassExtractEx {
+}
+
+class Person {
+    private String name;
+    private String street;
+    private String city;
+    private String detailAddress;
+
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return city + " " + street + "," + detailAddress;
+    }
+}
+
+
